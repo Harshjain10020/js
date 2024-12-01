e5e115337c1bcb86cf23928f0294e065d01c6788
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-01 17:15:11 +0000

    vid 5

---
+++ 01basic/02data.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+// alert("hello");
+// alert(3+3); // wee are using node not browser
+console.log(3+3);
+//readiablilty should be high
+
+//datatype
+let  name = "harsh"
+let age = 18
+let isLogged = null //false
+//string ""
+//num 2^53
+//boolean true / false
+//null standalone value ( soecial type ) no response got 
+//undefined -  value not decided yet 
+//symbol 
+
+//objects 
+console.log(typeof age);
+console.log(typeof isLogged);//object
+console.log(typeof undefined);
+
+
