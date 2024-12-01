4bd22a0fadb809bb8994d8b500a18cb14dc47750
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-01 17:40:03 +0000

    vid 6

--- 01basic/03con.js
+++ 01basic/03con.js
@@ -1,17 +1,17 @@
-// //conversion and operation
-// let age = null //"21a"
-// let score = 45
-// console.log(typeof age);
-// console.log(typeof (score));
-// let valueinnumber = Number(age)
-// console.log(typeof valueinnumber);
-// console.log(valueinnumber);//Nan special type 
-// let isLogged = ""
-// let booleanisloged = Boolean (isLogged)
-// console.log(booleanisloged);
-// //true = 1; flase = 0
-// // "" empty = flase
-// //"a" true 
+//conversion and operation
+let age = null //"21a"
+let score = 45
+console.log(typeof age);
+console.log(typeof (score));
+let valueinnumber = Number(age)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);//Nan special type 
+let isLogged = ""
+let booleanisloged = Boolean (isLogged)
+console.log(booleanisloged);
+//true = 1; flase = 0
+// "" empty = flase
+//"a" true 
 let somenum = 23
 let Stringnum = String(somenum)
 console.log(typeof Stringnum);
