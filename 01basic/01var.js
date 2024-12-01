c4a8614c48452cfe5436bb9c94b56f15e73f44d5
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-01 13:24:20 +0000

    vid 4

--- 01basic/01var.js
+++ 01basic/01var.js
@@ -1,7 +1,19 @@
-const accountId = 123;
-let accountEmail = "ff@await.com"
+const accountId = 123; // only way to declare const
+// two ways let var for variable ; less use var , due to scope {} things btw scope , earlier js doeswt work on scope
+let accountEmail = "ff@await.com"//solved scope wala prb
 var accPass = "1234"
-accountCity = "nagpur"
+accountCity = "nagpur" // how this worked 
+let accountState;//undefined 
+/*
+prefer not to use var bcoz of issue in block scope and functional scope */
+// accountId = 2 //doesnt allow "const"
+accountEmail = "asd@dsf.sdf"
+accPass = "123asda"
+accountCity = "delhi"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accPass);
+// console.log(accountCity);
 
-accountId = 2
-console.log(accountId);
+//u can use table 
+console.table({accountCity,accPass,accountEmail,accountState})
