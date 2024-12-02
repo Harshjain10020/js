a98c2690984ef2e63fd5c5a616f318d995582668
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-02 18:15:58 +0000

    vid 7

--- 01basic/03con.js
+++ 01basic/03con.js
@@ -1,20 +1,59 @@
-//conversion and operation
-let age = null //"21a"
-let score = 45
-console.log(typeof age);
-console.log(typeof (score));
-let valueinnumber = Number(age)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);//Nan special type 
-let isLogged = ""
-let booleanisloged = Boolean (isLogged)
-console.log(booleanisloged);
-//true = 1; flase = 0
-// "" empty = flase
-//"a" true 
-let somenum = 23
-let Stringnum = String(somenum)
-console.log(typeof Stringnum);
+// //conversion and operation
+// let age = null //"21a"
+// let score = 45
+// console.log(typeof age);
+// console.log(typeof (score));
+// let valueinnumber = Number(age)
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);//Nan special type 
+// let isLogged = ""
+// let booleanisloged = Boolean (isLogged)
+// console.log(booleanisloged);
+// //true = 1; flase = 0
+// // "" empty = flase
+// //"a" true 
+// let somenum = 23
+// let Stringnum = String(somenum)
+// console.log(typeof Stringnum);
+//******************************operations */
+let val = 3
+let negval = -val
+// console.log(negval);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+// let str3 = str1 + str2
+// // console.log(str3);
+// console.log("1"+2);
+// console.log(1+ "2");
+// console.log(1+2+ "2");
+// console.log("1"+2+2);
+
+
+// console.log(+true);//1
+// // console.log(true+);//error
+// console.log(+"");
+
+// let num1, num2 , num3
+// num1 = num2 = num3 = 2+2// dont do this
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
 
 
 
