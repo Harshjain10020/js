9a25ba299d5ae9e285762475e11dc5846df5affe
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-03 15:32:03 +0000

    Memory in javascript

--- 01basic/datatype_sum.js
+++ 01basic/datatype_sum.js
@@ -33,3 +33,43 @@ const myfunc = function () {
 console.log(typeof heros);
 console.log(typeof myfunc);
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+////////=============================
+//stack heap memory 
+//primitive type = stack memory  we get a copy of value 
+// non primitive heap // 
+
+// Memory in javascript
+// stack(Primitive data)(in stack there is copy in memory and change easily also the previous will not change) 
+// heap (non primitive || reference (array , function, object)) (they use heap memory and have the reference if we update any thing both will changes)
+
+// let x = 99;
+// let y = x
+// y = 199;
+// console.log(x)    // in this case primitve data tpye and use stack memory (changes is copy data not original)
+// console.log(y)
+
+let user1 = {
+    email : "hh@WebGLProgram.ss",
+    upi : "9asdaweq@ybl"
+}
+let user2 = user1
+user2.email = "aharsh@gash.csda"
+
+console.log(user1);
+console.log(user2);
+
+// let nums = {
+// name: "dushyant",
+// height: "183cm",
+// networth: 348764123473816,
+// }
+// let newnums = nums;
+
+// newnums.height = "191cm"
+// newnums.networht = 574578326587245;
+
+// console.table([nums.name , nums.height , nums.networht])                   // this is non primitive data type and heap is used (reference changes in orginal data)
+// console.table([newnums.name , newnums.height , newnums.networht])
