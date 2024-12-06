4082715a1d08dc728bd7190a93a37432208bad79
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-06 18:37:51 +0000

    vid 12

---
+++ 01basic/05str.js
@@ -0,0 +1,7 @@
+const name = "harsh"
+const repo = 10
+// console.log(name +" "+ repo); //old method
+console.log(`hello my name is ${name} and my repo count is ${repo}`); // ```````````````` not '''''''''''
+
+const gameName = new String('harsh');
+
