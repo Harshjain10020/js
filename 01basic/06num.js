04623ae7a6c46ea34bf26621533648949367fe91
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-06 18:56:28 +0000

    vid 13

--- 01basic/06num.js
+++ 01basic/06num.js
@@ -14,16 +14,23 @@ const hundreds = 1000000
 ///////////////maths////////////////////////
 
 // console.log(Math);
-// console.log(Math.abs(-1));//absolute value 
-// console.log(Math.round(5.32));
-// console.log(Math.ceil(5.32));
-// console.log(Math.floor(5.32));
-// console.log(Math.max(2,3,4));
-// console.log(Math.min(2,3,4));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));          // next greater value of the given number 
+// console.log(Math.floor(4.9));         // just lower value of the given number
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 99, 8));
 
-// console.log(Math.random());// 0 to 1 
-// console.log((Math.random()*10)+1);// 1 to 9 , 1 add to avoid 0.1
-// console.log((Math.floor(Math.random()*10)+1));// 1 to 9
+
+// console.log(Math.random());     // always give number between 0 to 1  (always give differ output)
+// console.log((Math.random()*10) + 1);    // to avoid the case of 0 we use the puls one 
+
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)   // now the range 10 to 20 , every time we got differ output
+
 
 const min = 10
 const max = 20
---
+++ 01basic/07date.js
@@ -0,0 +1,28 @@
+//Dates 
+let myDate = new Date();
+// console.log(myDate);//2024-12-06T18:46:28.041Z
+// console.log(myDate.toDateString());//Fri Dec 06 2024
+// console.log(myDate.toISOString());//2024-12-06T18:46:28.041Z
+// console.log(myDate.toLocaleDateString());//2/6/2024
+// console.log(myDate.toLocaleString());//12/6/2024, 6:46:28 PM
+
+// let myCreatdate = new Date(2024, 6, 29,5,3);
+let myCreatdate = new Date("2023-01-14");
+// console.log(myCreatdate.toDateString());
+// console.log(myCreatdate.toLocaleString());
+// console.log(myCreatdate.toLocaleString());
+let mytimestamp = Date.now()
+// console.log(mytimestamp);
+// console.log(myCreatdate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
+
+console.log(newDate)
+
+
