027cf1a6f29fc3c81dddf975fcd53b28e483448e
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-07 17:40:30 +0000

    vid 14

---
+++ 02basics/01array.js
@@ -0,0 +1,42 @@
+const myarray1 = [0,1,1,2,3,4,true , "hi"]
+const myarray = [0,1,1,2,3,4,]
+//resizable
+//can have mix type
+//array cannot be using arbitary string , only access thr u arr[i] , not arr["a"]
+//0 indexing 
+// change in copied array will have changes in mainn array 
+const myHero = ["daredevil", 'batman']
+
+const myarr3 =  new Array(1,2,3,4)
+// console.log(myarr3[3]);
+
+//array methods 
+// myarray.push(6)
+// myarray.push(7)
+// myarray.pop()
+// myarray.unshift(9)
+// myarray.shift()
+
+// console.log(myarray.includes(9));//boolean typ 
+// console.log(myarray.indexOf(9));//index typ 
+// const newArr = myarray.join()
+// console.log(myarray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+//slice , spilce 
+
+// console.log("A ", nums);           
+
+// const myn1 = nums.slice(1, 3)          //  use to get the element according to the range without changing the original array
+
+// console.log(myn1);
+// console.log("B ", nums);
+
+
+// const myn2 = nums.splice(1, 3)       //  use to get the element according to the range(also includes the last of range) and changing the original array
+// console.log("C ", nums);
+// console.log(myn2);
+
+
