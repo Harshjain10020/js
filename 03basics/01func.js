cd19d7e32da46653da87b2c5b351c5ade74db4db
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2024-12-21 20:04:35 +0000

    vid 21

--- 03basics/01func.js
+++ 03basics/01func.js
@@ -1,86 +1,86 @@
-// // // // console.log(1);
-// // // function saymyname(){
-// // //     console.log("Harsh");
+// // // // // console.log(1);
+// // // // function saymyname(){
+// // // //     console.log("Harsh");
+    
+// // // // }
+// // // // // saymyname;//reference 
+// // // // saymyname(); //exceute 
+
+// // // function add(num1, num2){
+// // //     console.log(num1 + num2);//Nan 
     
 // // // }
-// // // // saymyname;//reference 
-// // // saymyname(); //exceute 
+// // // add();//Nan no agrument 
+// // // add(3,7)
+// // // add(3,"7") // conevrt str 
+// // // add(3,"a") // conevrt str 
+// // // add(3,null) // conevrt str 
+// // // function add(num1, num2){
+// // // //    const res = console.log(num1 + num2);
+// // // //     return res;
+// // // return console.log(num1 + num2);
+// // // }
+// // // add(2,2)
 
-// // function add(num1, num2){
-// //     console.log(num1 + num2);//Nan 
-    
+
+// // function loginUserMessage(username = "john doe // default value if undefined it auto assign"){
+// //         // if(!username){
+// //         //     console.log("PLease enter a username");
+// //         //     return
+// //         if(username == undefined){
+// //             console.log("PLease enter a username");
+// //             return
+// //         }
+// //         return `${username} just logged in`
+// //     }
+// //     console.log(loginUserMessage("harsh"))               //  THE DEAFAULT WE CAN SET EASILY
+// //     // console.log(loginUserMessage("yakuxa"))
+// //     // console.log(loginUserMessage("the game  "))
+
+// //shopping cartt , user just add with no limit 
+
+// // function calCartPrice(...num1){
+// //     return num1
 // // }
-// // add();//Nan no agrument 
-// // add(3,7)
-// // add(3,"7") // conevrt str 
-// // add(3,"a") // conevrt str 
-// // add(3,null) // conevrt str 
-// // function add(num1, num2){
-// // //    const res = console.log(num1 + num2);
-// // //     return res;
-// // return console.log(num1 + num2);
+// // console.log(calCartPrice(200, 400, "HArhs" ));
+// // //rest operator ... or spread operator it will add more if user adds more 
+
+
+// // function calCartPrice(var1, var2, ...num1){
+// //     //here var1 = 200 , var2 = 400
+// //     console.log(var1);
+    
+// //     return num1
 // // }
-// // add(2,2)
+// // console.log(calCartPrice(200, 400, "HArhs" ));
 
 
-// function loginUserMessage(username = "john doe // default value if undefined it auto assign"){
-//         // if(!username){
-//         //     console.log("PLease enter a username");
-//         //     return
-//         if(username == undefined){
-//             console.log("PLease enter a username");
-//             return
-//         }
-//         return `${username} just logged in`
-//     }
-//     console.log(loginUserMessage("harsh"))               //  THE DEAFAULT WE CAN SET EASILY
-//     // console.log(loginUserMessage("yakuxa"))
-//     // console.log(loginUserMessage("the game  "))
 
-//shopping cartt , user just add with no limit 
 
-// function calCartPrice(...num1){
-//     return num1
+
+
+
+
+// //object 
+// const user = {
+//     username : "Harhs",
+//     price: " 22020202020202020202020202020"
 // }
-// console.log(calCartPrice(200, 400, "HArhs" ));
-// //rest operator ... or spread operator it will add more if user adds more 
 
-
-// function calCartPrice(var1, var2, ...num1){
-//     //here var1 = 200 , var2 = 400
-//     console.log(var1);
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
     
-//     return num1
 // }
-// console.log(calCartPrice(200, 400, "HArhs" ));
 
+// // handleObject(user)
+// handleObject({
+//     username : "yakuza", price: "399"
+// })
 
+// const myNewarray = [ 200,2003,300]
 
-
-
-
-
-
-//object 
-const user = {
-    username : "Harhs",
-    price: " 22020202020202020202020202020"
-}
-
-function handleObject(anyObject){
-    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
-    
-}
-
-// handleObject(user)
-handleObject({
-    username : "yakuza", price: "399"
-})
-
-const myNewarray = [ 200,2003,300]
-
-function returnArray(getArray){
-    return getArray[2]
-}
-// console.log(returnArray(myNewarray));
-console.log(returnArray([ 200,2003,300]));
+// function returnArray(getArray){
+//     return getArray[2]
+// }
+// // console.log(returnArray(myNewarray));
+// console.log(returnArray([ 200,2003,300]));
---
+++ 03basics/02scope.js
@@ -0,0 +1,22 @@
+
+//var c = 300                   // var can't able to differ global and block scope
+// let a = 300
+// if (true) {
+//     let a = 10                     // block or local scope the declare inside {} of condition or function etc
+//     const b = 20
+//     // console.log("INNER: ", a);
+    
+// }
+// console.log(a);               //  global scope just like global variable in cpp 
+// console.log(b);
+// console.log(c);
+//{}anything between scope
+var c = 300
+if(true){
+    let a= 10
+const b = 20
+var c = 20// this is global can be acces anywher 
+}
+
+// console.log(c);
+// console.log(b);
