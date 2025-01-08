02aee9cc0e48d95b32a633d8811a9be37097bef4
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-01-08 17:42:18 +0000

    vid 28

---
+++ 05loop/while.js
@@ -0,0 +1,23 @@
+//  +++++++++++++++++++++++++ while and do while ++++++++++++++++++++++++++++
+
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// let nums = ["flash", "batman", "superman"]
+// let x = 0
+// while (x < nums.length) {
+//     console.log(`Value is ${nums[x]}`);
+//     x = x + 1
+// }
+
+
+// let score = 100
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
