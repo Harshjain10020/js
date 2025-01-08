9c07f27fccd1cbbefd2dd9e2d39df6512be1f70d
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-01-08 17:31:41 +0000

    vid 26

---
+++ 05loop/for.js
@@ -0,0 +1,53 @@
+// //for
+// // for (let i = 0; i <= 10; i++) {
+// //     const element = i;
+// //     if (element == 5) {
+// //         console.log("5 is bad number");
+// //     }
+// //     console.log(element);
+    
+// // }
+// // //console.log(element);
+
+// for (let i = 0; i < 5; i++) {
+//     //console.log(`Outer loop value : ${i}`);
+    
+//     for (let j = 0; j < 5; j++) {
+        
+//         // console.log(`inner loop value : ${j}`);
+//         console.log(i+'*'+j+'=' + i*j);
+ // process.stdout.write('*');                          //  use process.stdout.write() to print the pattern instead console.log()
+        
+//     }
+    
+// }
+
+
+// let myArray = ["a", "b", "C"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${i}`)
+    
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//    console.log(`Value of i is ${i}`);
+// }
+
