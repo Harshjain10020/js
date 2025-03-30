13bd67e44b51af91aa47695863473577a6c67903
Harsh Jain <127287026+Harshjain10020@users.noreply.github.com>
2025-03-30 22:06:08 +0530

    #project_2

---
+++ projects/02_bmi_index/script.js
@@ -0,0 +1,22 @@
+
+// const weight = document.querySelector('#weight') this will give empty valyue at start 
+const form = document.querySelector('form')
+form.addEventListener('submit',  function(e){
+  e.preventDefault()
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const result = document.querySelector('#result');
+
+  if(height=== ''|| height <0 ||isNaN(height) ){
+    result.innerHTML = `Please give valid height ${height}`;
+  }else 
+
+  if(weight=== ''|| weight <0 ||isNaN(weight) ){
+    result.innerHTML = `Please give valid weight ${weight}`;
+  }else{
+    const bmi = (weight/ ((height**2)/10000)).toFixed(2)
+    //show result 
+    result.innerHTML = `<span>${bmi}</span>`
+  }
+
+})
